42407c84cb0c1fe6db1af52ca8651bdd88ff32f0
LeonardoFurmann <leohitnerf@gmail.com>
2023-05-08 17:31:21 -0300

    Update JogadorGUI.java

--- BlackJack/src/GUI_PAC/JogadorGUI.java
+++ BlackJack/src/GUI_PAC/JogadorGUI.java
@@ -3,8 +3,11 @@ package GUI_PAC;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
@@ -96,70 +99,183 @@ public  class JogadorGUI extends VJogadorAposta implements Displayable{
 		
 	}
 	
-	private class JogadorView extends JPanel implements JogadorListener{
+	private class JogadorView extends JPanel implements JogadorListener, ActionListener{
 		
-		private TitledBorder border;
+		private JButton apostar10 = new JButton("$10");
+		private JButton apostar50 = new JButton("$50");
+		private JButton apostar100 = new JButton("$100");
+		private JButton jogar = new JButton("Novo jogo");
+		private JButton sair = new JButton("Sair");
+		private JButton novaCarta = new JButton("Nova Carta");
+		private JButton passar = new JButton("Passar");
+		private JButton dobrar = new JButton("Dobrar Aposta");
 		
-		private ArrayList<VCarta> cartas = new ArrayList();
+		private final String NOVO_JOGO = "novo";
+		private final String SAIR = "sair";
+		private final String NOVA_CARTA = "NCarta";
+		private final String PASSAR = "passar";
+		private final String APOSTAR_10 = "apostar10";
+		private final String APOSTAR_50 = "apostar50";
+		private final String APOSTAR_100 = "apostar100";
+		private final String DOBRAR = "dobrar";
+		
+		private final Color FORREST_GREEN = new Color(35,142,35);
+		
+		public JogadorView() {
+			super(new BorderLayout());
+			JogadorView.this.addListener(this);
+			buildGUI();
+		}
 
-//		public JogadorView(VMao mao) {
-//			super(new FlowLayout(FlowLayout.LEFT));
-//			border = new TitledBorder(JogadorGUI.this.getNome());
-//			setBorder(border);
-//			setBackground(new Color(35,142,35));
-//			border.setTitleColor(Color.black);
-//			add(mao.view());
-//			repaint();
-//		}
+		private void buildGUI() {
+			JPanel controleApostas = new JPanel();
+			JPanel controleJogo= new JPanel();
+			
+			add(controleApostas, BorderLayout.NORTH);
+			add(controleJogo, BorderLayout.SOUTH);
+			
+			controleApostas.setBackground(FORREST_GREEN);
+			controleJogo.setBackground(FORREST_GREEN);
+			jogar.setActionCommand(NOVO_JOGO);
+			jogar.addActionListener(this);
+			sair.setActionCommand(SAIR);
+			sair.addActionListener(this);
+			novaCarta.setActionCommand(NOVA_CARTA);
+			novaCarta.addActionListener(this);
+			passar.setActionCommand(PASSAR);
+			passar.addActionListener(this);
+			apostar10.setActionCommand(APOSTAR_10);
+			apostar10.addActionListener(this);
+			apostar50.setActionCommand(APOSTAR_50);
+			apostar50.addActionListener(this);
+			apostar100.setActionCommand(APOSTAR_100);
+			apostar100.addActionListener(this);
+			dobrar.setActionCommand(DOBRAR);
+			dobrar.addActionListener(this);
+			
+			controleApostas.add(apostar10);
+			controleApostas.add(apostar50);
+			controleApostas.add(apostar100);
+			controleJogo.add(jogar);
+			controleJogo.add(novaCarta);
+			controleJogo.add(passar);
+			controleJogo.add(sair);
+			controleJogo.add(dobrar);
+			
+			enableControleApostas(false);
+			enableControleJogador(false);
+			enableDobrarAPosta(false);
+			//enableControleJogo(false);
+			
+		}
+
+		private void enableDobrarAPosta(boolean b) {
+			dobrar.setEnabled(b);
+			
+		}
+
+		private void enableControleJogador(boolean b) {
+			novaCarta.setEnabled(b);
+			passar.setEnabled(b);
+			
+		}
+
+		private void enableControleApostas(boolean b) {
+			apostar10.setEnabled(b);
+			apostar50.setEnabled(b);
+			apostar100.setEnabled(b);
+			
+		}
 		
-		@Override
-		public void jogadorMudou(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome);
-			repaint();
+		private void enableControleJogo(boolean b) {
+			jogar.setEnabled(b);
+			sair.setEnabled(b);
+			
 		}
 
 		@Override
+		public void actionPerformed(ActionEvent event) {
+			if (event.getActionCommand().equals(SAIR)) {
+				System.exit(0);
+			}else if (event.getActionCommand().equals(NOVA_CARTA)) {
+				enableDobrarAPosta(false);
+				takeCarta();
+			}else if (event.getActionCommand().equals(PASSAR)) {
+				enableDobrarAPosta(false);
+				ficar();
+			}else if (event.getActionCommand().equals(NOVO_JOGO)) {
+				enableDobrarAPosta(false);
+				enableControleJogo(false);
+				enableControleJogador(false);
+				enableControleApostas(false);
+				dealer.novoJogo();
+			}else if (event.getActionCommand().equals(APOSTAR_10)) {
+				enableDobrarAPosta(true);
+				enableControleJogador(false);
+				enableControleApostas(false);
+				apostar10();
+			}else if (event.getActionCommand().equals(APOSTAR_50)) {
+				enableDobrarAPosta(true);
+				enableControleJogador(false);
+				enableControleApostas(false);
+				apostar50();
+			}else if (event.getActionCommand().equals(APOSTAR_100)) {
+				enableDobrarAPosta(true);
+				enableControleJogador(false);
+				enableControleApostas(false);
+				apostar100();
+			}else if (event.getActionCommand().equals(DOBRAR)) {
+				enableDobrarAPosta(false);
+				enableControleJogador(false);
+				dobrarAposta();
+			}
+				
+			
+		}
+
+		@Override
+		public void jogadorMudou(Jogador jogador) {	}
+
+		@Override
 		public void jogadorEstourou(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "ESTOUROU");
-			repaint();
+			enableControleJogador(false);
+			enableControleJogo(true);
+			
 		}
 
 		@Override
 		public void jogadorBlackjack(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "BLACKJACK");
-			repaint();
+			enableDobrarAPosta(false);
+			enableControleJogador(false);
+			enableControleJogo(true);
+			
 		}
 
 		@Override
 		public void jogadorPassou(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "Passou...");
-			repaint();
+			enableControleJogador(false);
+			enableControleJogo(true);
 		}
 
 		@Override
 		public void jogadorGanhou(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "GANHOU");
-			repaint();
+			enableControleJogador(false);
+			enableControleJogo(true);
 		}
 
 		@Override
 		public void jogadorPerdeu(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "Perdeu...");
-			repaint();
+			enableControleJogador(false);
+			enableControleJogo(true);
+			enableDobrarAPosta(false);
 		}
 
 		@Override
 		public void jogadorSaiu(Jogador jogador) {
-			String nome = JogadorGUI.this.getNome();
-			border.setTitle(nome + "Saiu...");
-			repaint();
+			enableControleJogador(false);
+			enableControleJogo(true);
 		}
+		
 		
 		
 	}
